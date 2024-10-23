15e901c7f12d41a63fccb8dc3ae5672c7186d42b
khizar925 <khizarqamar06@gmail.com>
2024-10-23 22:20:09 +0500

    adding axios lib

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT;
 
@@ -11,14 +12,21 @@ const { adminRouter } = require("./routes/admin");
 const app = express();
 app.use(express.json());
 
+app.use(express.static("public"));
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html")); // Adjust this path as needed
+});
+
+
 async function main() {
-  await mongoose.connect(process.env.DATABASE_URL)
+  await mongoose.connect(process.env.DATABASE_URL);
   app.listen(PORT);
-  console.log("listening on port " + PORT)
+  console.log("listening on port " + PORT);
 }
 
-main()
+main();
